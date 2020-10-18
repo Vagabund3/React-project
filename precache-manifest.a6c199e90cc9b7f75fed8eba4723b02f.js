88c563afc5592b0a366d490504ac4f92a0c97a84
Vagabund3 <64327126+Vagabund3@users.noreply.github.com>
2020-10-18 23:20:20 +0300

    Updates

---
+++ precache-manifest.a6c199e90cc9b7f75fed8eba4723b02f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df4876d18586e662e6f587aba82d956",
+    "url": "/React-project/index.html"
+  },
+  {
+    "revision": "3ae1113e3056d05f390b",
+    "url": "/React-project/static/css/3.7e59fc17.chunk.css"
+  },
+  {
+    "revision": "27a1b28152da289ec979",
+    "url": "/React-project/static/css/4.7f68945e.chunk.css"
+  },
+  {
+    "revision": "a075fb8586c4cfa6267d",
+    "url": "/React-project/static/css/main.9efb59d5.chunk.css"
+  },
+  {
+    "revision": "f56e9740615248ccd946",
+    "url": "/React-project/static/js/2.10c8fb74.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-project/static/js/2.10c8fb74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae1113e3056d05f390b",
+    "url": "/React-project/static/js/3.e96e43d0.chunk.js"
+  },
+  {
+    "revision": "27a1b28152da289ec979",
+    "url": "/React-project/static/js/4.35cbcae6.chunk.js"
+  },
+  {
+    "revision": "a075fb8586c4cfa6267d",
+    "url": "/React-project/static/js/main.49d91956.chunk.js"
+  },
+  {
+    "revision": "5430e059f1f6e025035b",
+    "url": "/React-project/static/js/runtime-main.795a3329.js"
+  },
+  {
+    "revision": "739297c3ec0f727c32c8135aca85df15",
+    "url": "/React-project/static/media/UsersAva.739297c3.png"
+  },
+  {
+    "revision": "5ea4a24cc430c7f11dd5d3e54c03d165",
+    "url": "/React-project/static/media/preloader.5ea4a24c.svg"
+  }
+]);
